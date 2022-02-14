31d11eb167ff8e037db6d2c9f7c620af83859eb7
Rostoslav Telitsin <rostislav.telitsin@gmail.com>
2022-02-14 19:01:56 +0700

    add

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    private final static Integer INPUTS[][] = {
+            {-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20},
+            {-6, -3, -2, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20},
+            {-4, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20},
+            {-6, -5, -4, -3, -2, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20, 100, 101, 102, 103, 105},
+            {1, 3, 4, 6, 7, 9, 20, 21}
+    };
+
+    private final static String[] RESULTS = {
+            "-6,-3-1,3-5,7-11,14,15,17-20",
+            "-6,-3,-2,0,1,3-5,7-11,14,15,17-20",
+            "-4-1,3-5,7-11,14,15,17-20",
+            "-6--2,0,1,3-5,7-11,14,15,17-20,100-103,105",
+            "1,3,4,6,7,9,20,21"
+    };
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < INPUTS.length; i++){
+            var result = convertToIntervals(INPUTS[i]);
+            var expectedResult = RESULTS[i];
+            boolean correct = result.equalsIgnoreCase(expectedResult);
+            System.out.println("The result is " + (correct ? "correct. " : "incorrect. ") + "Result:" + result + " ExpectedResult:" + expectedResult);
+        }
+
+    }
+
+    private static String convertToIntervals(Integer[] input) {
+        String myResult = "";
+        boolean isSequence;
+        int i =0;
+        int first = 0;
+        int last = 0;
+
+        while (i< input.length) {
+            isSequence = true;
+            while (isSequence) {
+                if (i==input.length-1){
+                    last = i;
+                    isSequence = false;
+                } else if (input[i]==input[i+1]-1) {
+                    i++;
+                    last++;
+                } else {
+                    isSequence = false;
+                }
+            }
+
+            if (first==last) {
+                myResult = myResult + "," + String.valueOf(input[first]);
+            } else if (last-first > 1) {
+                myResult = myResult + "," + String.valueOf(input[first])+ "-"+ String.valueOf(input[last]);
+            } else {
+                myResult = myResult + "," + String.valueOf(input[first])+ ","+ String.valueOf(input[last]);
+            }
+
+            i++;
+            first = i;
+            last = i;
+        }
+
+        return myResult.substring(1);
+    }
+
+
 }
